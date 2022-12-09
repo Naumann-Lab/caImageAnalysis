37c87eb0ce6a137f8807341b4e3f9d546b0f6ea1
Matthew Loring <matthew.loring@duke.edu>
2022-12-09 11:26:16 -0500

    add tolerant mean util

--- utilties/arrutils.py
+++ utilties/arrutils.py
@@ -2,4 +2,24 @@ import numpy as np
 
 
 def pretty(x, n=3):
+    """
+    runs a little smoothing fxn over the array
+    :param x: arr
+    :param n: width of smooth
+    :return: smoothed arr
+    """
     return np.convolve(x, np.ones(n) / n, mode="same")
+
+
+def tolerant_mean(arrs):
+    """
+    takes an average of arrays of different lengths
+    :param arrs: N * arrs
+    :return: mean arr, std arr
+    """
+    lens = [len(i) for i in arrs]
+    arr = np.ma.empty((np.max(lens), len(arrs)))
+    arr.mask = True
+    for idx, l in enumerate(arrs):
+        arr[: len(l), idx] = l
+    return arr.mean(axis=-1), arr.std(axis=-1)
